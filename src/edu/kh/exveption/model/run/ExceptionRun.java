f331fff8c8099a7c7d8e39b35796556b25a62164
kdhleppa <newkdh8624@gmail.com>
2023-07-28 17:06:24 +0900

    m

---
+++ src/edu/kh/exveption/model/run/ExceptionRun.java
@@ -0,0 +1,17 @@
+package edu.kh.exveption.model.run;
+
+import edu.kh.exveption.model.service.ExceptionService;
+
+public class ExceptionRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		ExceptionService service = new ExceptionService();
+		
+		
+		service.ex4();
+	}
+
+}
